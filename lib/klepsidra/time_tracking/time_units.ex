38617f69667329da7e8565d84fb970b819713902
sacha <sacha@sachamasry.com>
2024-04-01 20:43:36 +0000

    Improved function documentation in `TimerUnits` module, adding DocTests to the `construct_duration_unit_options_list\1` function.
    FossilOrigin-Name: a081f5beed322a25dc32f9c47c7417d80036449cc61fd671152a15eb3014599e

--- lib/klepsidra/time_tracking/time_units.ex
+++ lib/klepsidra/time_tracking/time_units.ex
@@ -14,7 +14,9 @@ defmodule Klepsidra.TimeTracking.TimeUnits do
   value property.
 
   The returned value is a string, to be immediately usable, without further
-  conversion.
+  conversion. The default is stored, compiled, in the module attribute
+  `@default_billing_increment`, which will be supplanted in the future by
+  a user-defined choice, directly in the user interface.
   """
   @spec get_default_billing_increment() :: String.t()
   def get_default_billing_increment do
@@ -25,7 +27,35 @@ defmodule Klepsidra.TimeTracking.TimeUnits do
   Constructs a list of time units, ready to be used in an `options` input element.
 
   Returns list of tuples of the user-facing unit name and string version of the
-  time unit atom. For example, weeks would be presented as: `{"Weeks", "week"}`.
+  time unit atom, shaped for use in Phoenix-constructed [HTML] option input elements.
+  Each tuple has two elements, the first human-readable value, the second
+  is the string version of the time unit atom name.
+
+  For example, weeks would be presented as: `{"Weeks", "week"}`.
+
+  ## Examples
+
+      iex> Klepsidra.TimeTracking.TimeUnits.construct_duration_unit_options_list()
+      [
+        {"Minutes", "minute"},
+        {"5 min", "five_minute_increment"},
+        {"6 min", "six_minute_increment"},
+        {"10 min", "ten_minute_increment"},
+        {"12 min", "twelve_minute_increment"},
+        {"15 min", "fifteen_minute_increment"},
+        {"18 min", "eighteen_minute_increment"},
+        {"20 min", "twenty_minute_increment"},
+        {"24 min", "twenty_four_minute_increment"},
+        {"30 min", "thirty_minute_increment"},
+        {"36 min", "thirty_six_minute_increment"},
+        {"45 min", "fourty_five_minute_increment"},
+        {"60 min", "sixty_minute_increment"},
+        {"90 min", "ninety_minute_increment"},
+        {"2 hour increment", "one_hundred_twenty_minute_increment"}
+      ]
+
+      iex> Klepsidra.TimeTracking.TimeUnits.construct_duration_unit_options_list(use_primitives?: true)
+      [{"Seconds", "second"}, {"Minutes", "minute"}, {"Hours", "hour"}]
   """
   @spec construct_duration_unit_options_list() :: [{String.t(), String.t()}]
   def construct_duration_unit_options_list(opts \\ []) do
