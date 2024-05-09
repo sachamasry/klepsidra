355f10fd8a968595388d4162e7626bf728b0c0b5
sacha <sacha@sachamasry.com>
2024-05-09 23:55:23 +0000

    Updated position of tag selector on timer show view.
    Debugged behaviour of tag live component on addition of new tag and deletion of existing; corrected to ensure 
    both actions work correctly without reloading the view.
    
    Added display of tag background colour, if defined.
    
    FossilOrigin-Name: 6a5bec02a838b14809370e1481d45a05cfa627ccecb3d005e4118bb14a21c310

--- lib/klepsidra_web/live/tag_live/tag_search_form_component.ex
+++ lib/klepsidra_web/live/tag_live/tag_search_form_component.ex
@@ -15,44 +15,6 @@ defmodule KlepsidraWeb.Live.TagLive.SearchFormComponent do
   def render(assigns) do
     ~H"""
     <div class="tag-search">
-      <%!-- <form phx-submit="submit" phx-change="tag_search" phx-target={@myself}>
-        <input
-          type="text"
-          class="form-control"
-          name="search_phrase"
-          value={@search_phrase}
-          placeholder="Enter tag"
-          autocomplete="off"
-          phx-debounce="500"
-          list="matches"
-        /> --%>
-      <%!-- <button>Search</button> --%>
-
-      <%!-- <datalist id="matches">
-          <option :for={{id, name} <- @search_results} value={id}>
-            <%= name %>
-          </option>
-        </datalist> --%>
-      <%!-- </form> --%>
-
-      <%!-- <div>
-        <%= inspect(@search_results) %>
-      </div> --%>
-
-      <%!-- <div class="relative" phx-window-keydown="set-focus" phx-target={@myself}>
-        <div class="absolute z-50 left-0 right-0 rounded border-gray-100 shadow py-0 bg-white">
-          <div
-            :for={{{_id, search_result}, idx} <- Enum.with_index(@search_results)}
-            class={"cursor-pointer p-2 hover:bg-gray-200 focus:bg-gray-200 #{if(idx == @current_focus, do: "bg-gray-200")}"}
-            phx-click="pick"
-            phx-target={@myself}
-            phx-value-name={search_result}
-          >
-            <%= raw(format_search_result(search_result, @search_phrase)) %>
-          </div>
-        </div>
-      </div> --%>
-
       <form phx_submit="submit" phx_change="tag_search" phx-target={@myself}>
         <div
           class="py-2 px-3 bg-white border border-gray-400"
@@ -62,9 +24,9 @@ defmodule KlepsidraWeb.Live.TagLive.SearchFormComponent do
           <span
             :for={timer_tag <- @timer_tags}
             class="inline-block text-xs bg-green-400 text-white py-1 px-2 mr-1 mb-1 rounded"
+            style={if timer_tag.tag.colour, do: "background-color:" <> timer_tag.tag.colour}
           >
-            <%!-- <span><%= timer_tag.tag.name %></span> --%>
-            <div><%= inspect(timer_tag, pretty: true) %></div>
+            <span><%= timer_tag.tag.name %></span>
             <a
               href="#"
               class="text-white hover:text-white"
@@ -135,8 +97,6 @@ defmodule KlepsidraWeb.Live.TagLive.SearchFormComponent do
       Klepsidra.Categorisation.search_tags_by_name_prefix(search_phrase)
       |> Enum.map(fn tag -> {tag.id, tag.name} end)
 
-    # search_results = search(tags, search_phrase)
-
     socket =
       assign(socket,
         tags: tags,
@@ -148,13 +108,10 @@ defmodule KlepsidraWeb.Live.TagLive.SearchFormComponent do
   end
 
   def handle_event("pick", %{"name" => search_phrase}, socket) do
-    timer = socket.assigns.timer
-    timer_tags = add_tag_to_timer(timer, search_phrase)
-
-    # IO.inspect(timer_tags, label: "Timer tags")
+    timer = add_tag_to_timer(socket.assigns.timer, search_phrase)
 
     assigns = [
-      timer_tags: sort_tags(timer_tags),
+      timer_tags: sort_tags(timer.timer_tags),
       tags: [],
       search_results: [],
       search_phrase: ""
@@ -164,18 +121,14 @@ defmodule KlepsidraWeb.Live.TagLive.SearchFormComponent do
   end
 
   def handle_event("delete", %{"timer-tag-id" => timer_tag_id}, socket) do
-    # IO.inspect(timer_tag_id, label: "timer_tag_id")
-    # IO.inspect(socket.assigns, label: "Delete assigns")
-    # timer_tags = delete_tag_from_timer(socket.assigns, timer_tag_id)
-
-    # IO.inspect(socket.assigns.timer_tags, label: "timer_tags before deletion")
-    # timer_tags =
     delete_tag_from_timer(timer_tag_id)
 
-    # IO.inspect(timer_tags, label: "timer_tags ")
-    # IO.inspect(socket.assigns.timer_tags, label: "timer_tags after deletion")
+    timer_tags =
+      socket.assigns.timer_tags
+      |> Enum.reject(fn i -> i.id == String.to_integer(timer_tag_id) end)
+
     assigns = [
-      # timer_tags: timer_tags,
+      timer_tags: timer_tags,
       tags: [],
       search_results: [],
       search_phrase: ""
@@ -217,22 +170,6 @@ defmodule KlepsidraWeb.Live.TagLive.SearchFormComponent do
   # FALLBACK FOR NON RELATED KEY STROKES
   def handle_event("set-focus", _, socket), do: {:noreply, socket}
 
-  # defp search(_, ""), do: []
-
-  # defp search(tags, search_phrase) do
-  #   tags
-  #   |> Enum.map(fn tag -> tag.name end)
-  #   |> Enum.sort()
-  #   |> Enum.filter(fn tags -> matches?(tags, search_phrase) end)
-  # end
-
-  # defp matches?(first, second) do
-  #   String.starts_with?(
-  #     String.downcase(first),
-  #     String.downcase(second)
-  #   )
-  # end
-
   def format_search_result(search_result, search_phrase) do
     split_at = String.length(search_phrase)
     {selected, rest} = String.split_at(search_result, split_at)
@@ -244,30 +181,16 @@ defmodule KlepsidraWeb.Live.TagLive.SearchFormComponent do
     Categorisation.tag_timer(timer, %{tag: %{name: search_phrase}})
     timer = TimeTracking.get_timer!(timer.id) |> Klepsidra.Repo.preload(:tags)
 
-    timer.tags
+    # timer.tags
+    timer
   end
 
-  # defp delete_tag_from_timer(%{timer: timer, timer_tags: timer_tags}, timer_tag_id) do
   defp delete_tag_from_timer(timer_tag_id) do
-    # timer_tags
-    # |> dbg
-    # |> Enum.reject(fn tag ->
-    #   if "#{tag.id}" == timer_tag_id do
-    #     Categorisation.delete_tag_from_timer(timer, tags.tag)
-    #     true
-    #   else
-    #     false
-    #   end
-    # end)
-
-    # Klepsidra.Repo.get_by!(TimerTags, id: String.to_integer(timer_tag_id))
-
     {:ok, timer_tag} =
       timer_tag_id
       |> Categorisation.get_timer_tag!()
       |> Categorisation.delete_timer_tag()
 
-    # IO.inspect(timer_tag, label: "timer_tag deleted")
     timer_tag
   end
 
