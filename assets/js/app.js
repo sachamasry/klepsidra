9c8f831cb86e26fb6d13781b1769ad06d5607b63
sacha <sacha@sachamasry.com>
2024-08-18 23:34:40 +0000

    Refactored default delete confirmation dialog with a [styled, customisable and user-friendly confirmation modal](https://dev.to/neophen/beter-data-confirm-modals-in-phoenix-liveview-5al5).
    FossilOrigin-Name: 78ee13394a808e8cfdf71ccba3f446ed54730862184beb293419a9633058ae38

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,3 +64,59 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+// Default delete confirmation dialog override
+// Attribute which we use to re-trigger the click event
+const CONFIRM_ATTRIBUTE = "data-confirm-fired"
+
+// our dialog from the `root.html.heex`
+const DANGER_DIALOG = document.getElementById("danger_dialog");
+
+// Here we override the behaviour
+document.body.addEventListener('phoenix.link.click', function (event) {
+  // we prevent the default handling of this by phoenix html
+  event.stopPropagation();
+
+  // grab the target
+  const { target: targetButton } = event;
+  const title = targetButton.getAttribute("data-confirm");
+
+  // if the target does not have `data-confirm` we simply ignore and continue
+  if (!title) { return true; }
+
+  // For re-triggering the click event
+  if (targetButton.hasAttribute(CONFIRM_ATTRIBUTE)) {
+    targetButton.removeAttribute(CONFIRM_ATTRIBUTE)
+    return true;
+  }
+
+  // We do this since `window.confirm` prevents all execution by default.
+  // To recreate this behaviour we `preventDefault` 
+  // Then add an attribute which will allow us to re-trigger the click event while skipping the dialog
+  event.preventDefault();
+  targetButton.setAttribute(CONFIRM_ATTRIBUTE, "")
+
+  // Reset the `returnValue` as otherwise on keyboard `Esc` it will simply take the most recent `returnValue`, causing all sorts of issues :D
+  DANGER_DIALOG.returnValue = "cancel";
+
+  // We use the title, which is nice we can have translated titles
+  DANGER_DIALOG.querySelector("[data-ref='title']").innerText = title;
+
+
+  // <dialog> is a very cool element and provides a lot of cool things out of the box, like showing the modal in the #top-layer
+  DANGER_DIALOG.showModal();
+
+  // Re-triggering logic
+  DANGER_DIALOG.addEventListener('close', ({ target }) => {
+    if (target.returnValue === "confirm") {
+      // we re-trigger the click event
+      // since we have the attribute set. This will just execute the click event
+      targetButton.click();
+    } else {
+      // Remove the attribute on cancel as otherwise the next click would execute the click event without the dialog
+      targetButton.removeAttribute(CONFIRM_ATTRIBUTE);
+    }
+  // once: true, automatically remove the listener after first execution
+  }, { once: true })
+
+}, false)
+
