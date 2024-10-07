9e2fd297fc3e7db36709ea030f9fba9b4db13e38
sacha <sacha@sachamasry.com>
2024-10-07 01:56:08 +0000

    Completed all structured documentation of the `Klepsidra.Math` module, all unit and relevant (identity and zero) property tests.
    FossilOrigin-Name: 4cf4007d2db4b3e0723ad3d99152b5695dfc8926639c97721e18954a35553f78

--- lib/klepsidra/math.ex
+++ lib/klepsidra/math.ex
@@ -15,22 +15,29 @@ defmodule Klepsidra.Math do
   Calculates the arithmetic mean, usually referred to as the average,
   given a sum and count of items.
 
-  ## Comments
-
   As the functionality inevitably depends on division, eager pattern-matching
   is used in function heads, immediately returning zero, preventing divide by
   zero errors.
 
+  ## Arguments
+
+  * `sum` is any valid `Cldr.Unit`, integer or float, and is the numerator
+  * `count` is an integer only, used for a discrete number of events
+
+  ## Returns
+
+  * The arithmetic mean, in integer or float format
+
   ## Examples
 
       iex> Math.arithmetic_mean(Cldr.Unit.new!(91.0, :second), 13)
-      Cldr.Unit.new!(:second, 7.0)
+      Cldr.Unit.new!(:second, 7)
 
       iex> Math.arithmetic_mean(Cldr.Unit.new!(0, :second), 13)
-      Cldr.Unit.new!(:second, "0.0")
+      Cldr.Unit.new!(:second, 0)
 
       iex> Math.arithmetic_mean(Cldr.Unit.new!(7, :second), 0)
-      Cldr.Unit.new!(0, :second)
+      0
 
       iex> Math.arithmetic_mean(13, 2)
       6.5
@@ -46,13 +53,12 @@ defmodule Klepsidra.Math do
           sum :: number() | any(),
           count :: integer()
         ) :: number()
-  def arithmetic_mean(%Cldr.Unit{}, 0), do: Cldr.Unit.new!(0, :second)
+  def arithmetic_mean(%Cldr.Unit{} = _sum, 0), do: 0
   def arithmetic_mean(_sum, 0), do: 0
 
   def arithmetic_mean(%Cldr.Unit{} = sum, count)
       when is_integer(count) do
-    multi_unit_div(sum.value, count)
-    |> Cldr.Unit.new!(sum.unit)
+    multi_unit_div(sum, count)
   end
 
   def arithmetic_mean(sum, count)
@@ -60,13 +66,42 @@ defmodule Klepsidra.Math do
     multi_unit_div(sum, count)
   end
 
+  @doc """
+  Divides multiple types of units not directly covered in the `Kernel`,
+  `Decimal` and `Cldr.Unit` modules.
+
+  As division by zero is illegal, eager pattern-matching is used in
+  function heads, immediately returning zero or equivalent, preventing 
+  raising of errors.
+
+  ## Arguments
+
+  * `numerator`, which is a number, `Decimal`, or `Cldr.Unit` type
+  * `denominator`, a number or `Decimal` type
+
+  ## Returns
+
+  * Result of the division, in float, `Decimal`, or `Cldr.Unit` type
+
+  ## Examples
+
+      iex> Math.multi_unit_div(300, 13)
+      23.076923076923077
+  """
   @spec multi_unit_div(
-          numerator :: number() | Decimal.t(),
+          numerator :: number() | Decimal.t() | Cldr.Unit.t(),
           denominator :: number() | Decimal.t()
-        ) :: float() | Decimal.t()
+        ) :: float() | Decimal.t() | Cldr.Unit.t()
   def multi_unit_div(%Decimal{} = _numerator, 0), do: Decimal.new(0)
+  def multi_unit_div(%Decimal{} = _numerator, +0.0), do: Decimal.new("0.0")
   def multi_unit_div(_numerator, %Decimal{coef: 0}), do: Decimal.new(0)
   def multi_unit_div(_numerator, 0), do: 0.0
+  def multi_unit_div(_numerator, +0.0), do: 0.0
+
+  def multi_unit_div(%Cldr.Unit{} = numerator, denominator)
+      when is_integer(denominator) or is_struct(denominator, Cldr.Unit) do
+    Cldr.Unit.div!(numerator, denominator)
+  end
 
   def multi_unit_div(%Decimal{} = numerator, denominator)
       when is_float(denominator) do
--- test/klepsidra/math_test.exs
+++ test/klepsidra/math_test.exs
@@ -1,24 +1,22 @@
 defmodule Klepsidra.MathTest do
   use ExUnit.Case, async: true
+  use ExUnitProperties
 
   alias Klepsidra.Math
 
   doctest Klepsidra.Math
 
-  describe "arithmetic mean calculation" do
+  describe "arithmetic mean calculations: `Math.arithmetic_mean/2`" do
     test "calculate arithmetic mean between `Cldr.Unit` sum and integer count" do
-      assert Math.arithmetic_mean(Cldr.Unit.new!(3600, :second), 12) ==
-               Cldr.Unit.new!(:second, "300.0")
+      assert Math.arithmetic_mean(Cldr.Unit.new!(3600, :second), 12).value == 300.0
     end
 
     test "calculate arithmetic mean between zero `Cldr.Unit` sum and integer count" do
-      assert Math.arithmetic_mean(Cldr.Unit.new!(0, :second), 12) ==
-               Cldr.Unit.new!(:second, "0.0")
+      assert Math.arithmetic_mean(Cldr.Unit.new!(0, :second), 12).value == 0
     end
 
     test "calculate arithmetic mean between `Cldr.Unit` sum and integer zero count" do
-      assert Math.arithmetic_mean(Cldr.Unit.new!(3600, :second), 0) ==
-               Cldr.Unit.new!(:second, 0)
+      assert Math.arithmetic_mean(Cldr.Unit.new!(3600, :second), 0) == 0
     end
 
     test "calculate arithmetic mean between integer sum and integer count" do
@@ -36,9 +34,18 @@ defmodule Klepsidra.MathTest do
     test "calculate arithmetic mean between float sum and integer zero count" do
       assert Math.arithmetic_mean(36.63, 0) == 0
     end
+
+    property "checks identity and zero properties with Cldr.Unit numerator" do
+      check all(numerator <- integer(), numerator >= 0) do
+        unit_numerator = Cldr.Unit.new!(numerator, :second)
+
+        assert Math.arithmetic_mean(unit_numerator, 1) == unit_numerator
+        assert Math.arithmetic_mean(unit_numerator, 0) == 0
+      end
+    end
   end
 
-  describe "multi-unit division" do
+  describe "multi-unit division: `Math.multi_unit_div/2`" do
     test "divide integer with float" do
       assert Math.multi_unit_div(13, 2.6) == 5.0
     end
@@ -49,6 +56,7 @@ defmodule Klepsidra.MathTest do
 
     test "divide integer with zero" do
       assert Math.multi_unit_div(42, 0) == 0.0
+      assert Math.multi_unit_div(42, 0.0) == 0.0
     end
 
     test "divide float with float" do
@@ -61,6 +69,7 @@ defmodule Klepsidra.MathTest do
 
     test "divide float with zero" do
       assert Math.multi_unit_div(42.13, 0) == 0.0
+      assert Math.multi_unit_div(42.13, 0.0) == 0.0
     end
 
     test "divide decimal with float" do
@@ -73,6 +82,7 @@ defmodule Klepsidra.MathTest do
 
     test "divide decimal with zero" do
       assert Math.multi_unit_div(Decimal.new(42), 0) == Decimal.new(0)
+      assert Math.multi_unit_div(Decimal.new(42), 0.0) == Decimal.new("0.0")
     end
 
     test "divide integer with decimal float" do
@@ -99,6 +109,62 @@ defmodule Klepsidra.MathTest do
     test "divide float with decimal zero" do
       assert Math.multi_unit_div(42.13, Decimal.new(0)) == Decimal.new(0)
       assert Math.multi_unit_div(42.13, Decimal.new("0.0")) == Decimal.new(0)
+    end
+
+    property "checks `multi_unit_div/2` identity and zero properties with integer numerator" do
+      check all(numerator <- integer()) do
+        decimal_numerator = Decimal.new(numerator)
+        assert Math.multi_unit_div(numerator, 1) == numerator
+        assert Math.multi_unit_div(numerator, 1.0) == numerator
+        assert Math.multi_unit_div(numerator, 0) == 0
+        assert Math.multi_unit_div(numerator, 0.0) == 0
+        assert Math.multi_unit_div(decimal_numerator, 1) == decimal_numerator
+
+        assert Decimal.eq?(
+                 Math.multi_unit_div(decimal_numerator, 1.0),
+                 decimal_numerator
+               )
+
+        assert Decimal.eq?(
+                 Math.multi_unit_div(decimal_numerator, 0),
+                 0
+               )
+
+        assert Decimal.eq?(
+                 Math.multi_unit_div(decimal_numerator, 0.0),
+                 0
+               )
+      end
+    end
+
+    property "checks `multi_unit_div/2` identity and zero properties with float numerator" do
+      check all(numerator <- float()) do
+        decimal_numerator = Decimal.from_float(numerator)
+        assert Math.multi_unit_div(numerator, 1) == numerator
+        assert Math.multi_unit_div(numerator, 1.0) == numerator
+        assert Math.multi_unit_div(numerator, 0) == 0
+        assert Math.multi_unit_div(numerator, 0.0) == 0
+
+        assert Decimal.eq?(
+                 Math.multi_unit_div(decimal_numerator, 1),
+                 Decimal.new("#{numerator}")
+               )
+
+        assert Decimal.eq?(
+                 Math.multi_unit_div(decimal_numerator, 1.0),
+                 Decimal.new("#{numerator}")
+               )
+
+        assert Decimal.eq?(
+                 Math.multi_unit_div(decimal_numerator, 0),
+                 0
+               )
+
+        assert Decimal.eq?(
+                 Math.multi_unit_div(decimal_numerator, 0.0),
+                 0
+               )
+      end
     end
   end
 end
