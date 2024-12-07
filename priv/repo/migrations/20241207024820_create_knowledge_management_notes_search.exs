5c66217528adf49f10cd27d90a50ec097151fd9f
sacha <sacha@sachamasry.com>
2024-12-07 20:49:26 +0000

    Created a `knowledge_managment_notes` full-text search migration, responsible for creating the virtual table as well as the _on create_, _on update_ and _on delete_ triggers, needed to keep the index updated.
    FossilOrigin-Name: 05d1a7901c133c21c64ddecd2fabc8f96c8efe24b6b124dd98de330a05f45017

---
+++ priv/repo/migrations/20241207024820_create_knowledge_management_notes_search.exs
@@ -0,0 +1,44 @@
+defmodule Klepsidra.Repo.Migrations.CreateKnowledgeManagementNotesSearch do
+  use Ecto.Migration
+
+  def up do
+    execute("""
+    CREATE VIRTUAL TABLE IF NOT EXISTS knowledge_management_notes_search
+    USING fts5(id UNINDEXED, title, content, summary, status, tokenize='trigram');
+    """)
+
+    execute("""
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ai AFTER INSERT
+    ON knowledge_management_notes
+    BEGIN
+      INSERT INTO knowledge_management_notes_search(rowid, id, title, content, summary, status)
+      VALUES (NEW.rowid, NEW.id, NEW.title, NEW.content, NEW.summary, NEW.status);
+    END;
+    """)
+
+    execute("""
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ad AFTER DELETE
+    ON knowledge_management_notes
+    BEGIN
+      DELETE FROM knowledge_management_notes_search WHERE rowid = OLD.rowid;
+    END;
+    """)
+
+    execute("""
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_au AFTER UPDATE
+    ON knowledge_management_notes
+    BEGIN
+      UPDATE knowledge_management_notes_search
+      SET title = NEW.title, content = NEW.content, summary = NEW.summary, status = NEW.status
+      WHERE rowid = NEW.rowid;
+    END;
+    """)
+  end
+
+  def down do
+    execute("DROP TRIGGER IF EXISTS knowledge_management_notes_search_ai;")
+    execute("DROP TRIGGER IF EXISTS knowledge_management_notes_search_ad;")
+    execute("DROP TRIGGER IF EXISTS knowledge_management_notes_search_au;")
+    execute("DROP TABLE IF EXISTS knowledge_management_notes_search;")
+  end
+end
