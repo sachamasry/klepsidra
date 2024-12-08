7e5ee55779c32e545a1a1743b498902c33b26139
sacha <sacha@sachamasry.com>
2024-12-08 22:15:12 +0000

    Refactoring: changed the way that the `knowledge_management_notes_search` table is populated. When a new note is created, the contents of the relevant fields are inserted into the search table, with tags as an empty string; when the note itself is updated, the fields in the search table are updated, and when the note is deleted, the row in the search table is deleted also. With regards to note tags, when either a new tag is attached to a note, or removed, a full tags concatenation `SELECT` query is run, updating the 'tags' field in the notes search table. This is both more performant, as the main record is only created and deleted when it has to be, and tags are only recaclulated on addition and deletion, merely updating a single field.
    FossilOrigin-Name: 4b6c74349f0343d510415a8431deba8abdb02f0592b7e40d04db820d06667e44

--- priv/repo/migrations/20241207024820_create_knowledge_management_notes_search.exs
+++ priv/repo/migrations/20241207024820_create_knowledge_management_notes_search.exs
@@ -8,40 +8,40 @@ defmodule Klepsidra.Repo.Migrations.CreateKnowledgeManagementNotesSearch do
       tokenize='trigram remove_diacritics 1');
     """)
 
+    # When a new note is created, it will not have had any tags attached
+    # just yet; insert an empty string into the tag field
     execute("""
     CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ai
     AFTER INSERT ON knowledge_management_notes
     BEGIN
       INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
         summary, tags)
-      SELECT NEW.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
-      FROM knowledge_management_notes kmn
-      LEFT JOIN knowledge_management_note_tags kmnt
-      ON kmn.id = kmnt.note_id
-      LEFT JOIN tags t
-      ON kmnt.tag_id = t.id
-      WHERE kmn.id = NEW.id
-      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
+      VALUES (NEW.rowid, NEW.id, NEW.title, NEW.content, NEW.summary, '');
     END;
     """)
 
+    # A tag can only be attached to the knowledge_management_note_tags table
+    # after a note record has been created, so all that's needed is to find
+    # the correct note record in the search table, updating its 'tags' field
+    # value to the concatenation of all the tags assigned to the note
     execute("""
     CREATE TRIGGER IF NOT EXISTS knowledge_management_note_search_note_tags_ai
     AFTER INSERT ON knowledge_management_note_tags
     BEGIN
-      INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
-        summary, tags)
-      SELECT NEW.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
-      FROM knowledge_management_notes kmn
-      LEFT JOIN knowledge_management_note_tags kmnt
-      ON kmn.id = kmnt.note_id
-      LEFT JOIN tags t
-      ON kmnt.tag_id = t.id
-      WHERE kmn.id = NEW.note_id
-      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
+      UPDATE knowledge_management_notes_search
+      SET tags = (
+        SELECT GROUP_CONCAT(t.name, ' ⸱ ') tags
+        FROM knowledge_management_note_tags kmnt
+        LEFT JOIN tags t
+        ON kmnt.tag_id = t.id
+        WHERE kmnt.note_id = NEW.note_id
+      )
+      WHERE id = NEW.note_id;
     END;
     """)
 
+    # When an actual note is deleted, remove it completely from the search
+    # index
     execute("""
     CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ad
     AFTER DELETE ON knowledge_management_notes
@@ -50,41 +50,34 @@ defmodule Klepsidra.Repo.Migrations.CreateKnowledgeManagementNotesSearch do
     END;
     """)
 
+    # When a note is updated, update its title, content and summary fields
+    # only
     execute("""
     CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_au
     AFTER UPDATE ON knowledge_management_notes
     BEGIN
-      DELETE FROM knowledge_management_notes_search WHERE id = OLD.id;
-
-      INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
-        summary, tags)
-      SELECT NEW.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
-      FROM knowledge_management_notes kmn
-      LEFT JOIN knowledge_management_note_tags kmnt
-      ON kmn.id = kmnt.note_id
-      LEFT JOIN tags t
-      ON kmnt.tag_id = t.id
-      WHERE kmn.id = NEW.id
-      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
+      UPDATE knowledge_management_notes_search
+      SET title = NEW.title, content = NEW.content, summary = NEW.summary
+      WHERE rowid = NEW.rowid;
     END;
     """)
 
+    # When a tag is deleted, all that needs to be done is to rebuild the note's
+    # tag concatenation string, which will now be shorter by one item, and UPDATE
+    # the 'tags' field in the correct row in the search table
     execute("""
     CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_note_tags_ad
     AFTER DELETE ON knowledge_management_note_tags
     BEGIN
-      DELETE FROM knowledge_management_notes_search WHERE id = OLD.note_id;
-
-      INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
-        summary, tags)
-      SELECT OLD.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
-      FROM knowledge_management_notes kmn
-      LEFT JOIN knowledge_management_note_tags kmnt
-      ON kmn.id = kmnt.note_id
-      LEFT JOIN tags t
-      ON kmnt.tag_id = t.id
-      WHERE kmn.id = OLD.note_id
-      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
+      UPDATE knowledge_management_notes_search
+      SET tags = (
+        SELECT GROUP_CONCAT(t.name, ' ⸱ ') tags
+        FROM knowledge_management_note_tags kmnt
+        LEFT JOIN tags t
+        ON kmnt.tag_id = t.id
+        WHERE kmnt.note_id = OLD.note_id
+      )
+      WHERE id = OLD.note_id;
     END;
     """)
   end
