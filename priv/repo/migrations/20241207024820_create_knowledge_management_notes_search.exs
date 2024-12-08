8bae761e8b9688df04082f048daa56996dea89da
sacha <sacha@sachamasry.com>
2024-12-08 03:33:24 +0000

    Updated `KnowledgeManagementNotes` full-text search indexing to include note tags in a comma-separated list, providing note search on tag names as well. Updated existing, and added new triggers, ensuring that the entire note search record is updated with tag changes.
    FossilOrigin-Name: c868d262a869269e96b48db891c2e5d8d3750103c1b3e634ba61317cacf9e6bc

--- priv/repo/migrations/20241207024820_create_knowledge_management_notes_search.exs
+++ priv/repo/migrations/20241207024820_create_knowledge_management_notes_search.exs
@@ -4,40 +4,96 @@ defmodule Klepsidra.Repo.Migrations.CreateKnowledgeManagementNotesSearch do
   def up do
     execute("""
     CREATE VIRTUAL TABLE IF NOT EXISTS knowledge_management_notes_search
-    USING fts5(id UNINDEXED, title, content, summary, tokenize='trigram remove_diacritics 1');
+    USING fts5(id UNINDEXED, title, content, summary, tags,
+      tokenize='trigram remove_diacritics 1');
     """)
 
     execute("""
-    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ai AFTER INSERT
-    ON knowledge_management_notes
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ai
+    AFTER INSERT ON knowledge_management_notes
     BEGIN
-      INSERT INTO knowledge_management_notes_search(rowid, id, title, content, summary)
-      VALUES (NEW.rowid, NEW.id, NEW.title, NEW.content, NEW.summary);
+      INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
+        summary, tags)
+      SELECT NEW.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
+      FROM knowledge_management_notes kmn
+      LEFT JOIN knowledge_management_note_tags kmnt
+      ON kmn.id = kmnt.note_id
+      LEFT JOIN tags t
+      ON kmnt.tag_id = t.id
+      WHERE kmn.id = NEW.id
+      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
     END;
     """)
 
     execute("""
-    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ad AFTER DELETE
-    ON knowledge_management_notes
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_note_search_note_tags_ai
+    AFTER INSERT ON knowledge_management_note_tags
+    BEGIN
+      INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
+        summary, tags)
+      SELECT NEW.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
+      FROM knowledge_management_notes kmn
+      LEFT JOIN knowledge_management_note_tags kmnt
+      ON kmn.id = kmnt.note_id
+      LEFT JOIN tags t
+      ON kmnt.tag_id = t.id
+      WHERE kmn.id = NEW.note_id
+      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
+    END;
+    """)
+
+    execute("""
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_ad
+    AFTER DELETE ON knowledge_management_notes
     BEGIN
       DELETE FROM knowledge_management_notes_search WHERE rowid = OLD.rowid;
     END;
     """)
 
     execute("""
-    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_au AFTER UPDATE
-    ON knowledge_management_notes
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_au
+    AFTER UPDATE ON knowledge_management_notes
     BEGIN
-      UPDATE knowledge_management_notes_search
-      SET title = NEW.title, content = NEW.content, summary = NEW.summary
-      WHERE rowid = NEW.rowid;
+      DELETE FROM knowledge_management_notes_search WHERE id = OLD.id;
+
+      INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
+        summary, tags)
+      SELECT NEW.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
+      FROM knowledge_management_notes kmn
+      LEFT JOIN knowledge_management_note_tags kmnt
+      ON kmn.id = kmnt.note_id
+      LEFT JOIN tags t
+      ON kmnt.tag_id = t.id
+      WHERE kmn.id = NEW.id
+      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
+    END;
+    """)
+
+    execute("""
+    CREATE TRIGGER IF NOT EXISTS knowledge_management_notes_search_note_tags_ad
+    AFTER DELETE ON knowledge_management_note_tags
+    BEGIN
+      DELETE FROM knowledge_management_notes_search WHERE id = OLD.note_id;
+
+      INSERT INTO knowledge_management_notes_search(rowid, id, title, content,
+        summary, tags)
+      SELECT OLD.rowid, kmn.id, kmn.title, kmn.content, kmn.summary, GROUP_CONCAT(t.name, ', ') tags
+      FROM knowledge_management_notes kmn
+      LEFT JOIN knowledge_management_note_tags kmnt
+      ON kmn.id = kmnt.note_id
+      LEFT JOIN tags t
+      ON kmnt.tag_id = t.id
+      WHERE kmn.id = OLD.note_id
+      GROUP BY kmn.id, kmn.title, kmn.content, kmn.summary;
     END;
     """)
   end
 
   def down do
     execute("DROP TRIGGER IF EXISTS knowledge_management_notes_search_ai;")
+    execute("DROP TRIGGER IF EXISTS knowledge_management_note_search_note_tags_ai;")
     execute("DROP TRIGGER IF EXISTS knowledge_management_notes_search_ad;")
+    execute("DROP TRIGGER IF EXISTS knowledge_management_notes_search_note_tags_ad;")
     execute("DROP TRIGGER IF EXISTS knowledge_management_notes_search_au;")
     execute("DROP TABLE IF EXISTS knowledge_management_notes_search;")
   end
