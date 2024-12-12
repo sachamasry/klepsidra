a2c675b927c7e27f045dae7b9ce568390ed7c741
sacha <sacha@sachamasry.com>
2024-12-12 12:56:26 +0000

    Started refactoring `note_relationship_component`, building it up in a more modular manner, in preparation to display all relations to the note.
    FossilOrigin-Name: dd42bb085f7b69798467d76d67808a63f87e73a926c5aae7cd2acbfb6851424b

--- lib/klepsidra_web/live/notes_live/note_relationship_component.ex
+++ lib/klepsidra_web/live/notes_live/note_relationship_component.ex
@@ -10,19 +10,15 @@ defmodule KlepsidraWeb.NotesLive.NoteRelationshipComponent do
   def render(assigns) do
     ~H"""
     <div id="note-relationship-container">
-      <h3>Related notes</h3>
-      <.simple_form
-        for={@note_relation_form}
+      <.relationship_maker
+        title="Relate notes"
         id="note-relation-form"
+        for={@note_relation_form}
         phx-submit="save"
         phx-change="validate"
         phx-target={@myself}
         phx-value-id={@note_id}
-      >
-        <.input field={@note_relation_form[:target_note_id]} type="text" autocomplete="off" />
-        <.input field={@note_relation_form[:relationship_type_id]} type="text" autocomplete="off" />
-        <.button phx-disable-with="Saving note...">Relate notes</.button>
-      </.simple_form>
+      />
     </div>
     """
   end
@@ -103,4 +99,26 @@ defmodule KlepsidraWeb.NotesLive.NoteRelationshipComponent do
   end
 
   defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+
+  attr(:title, :string, default: "Relate notes")
+  attr(:id, :string, required: true, doc: "The ID of the relation-definer form")
+  attr(:for, :any, required: true, doc: "The datastructure for the form")
+
+  attr(:rest, :global,
+    include: ~w(autocomplete name rel action enctype method novalidate target),
+    doc: "Arbitrary HTML and Phoenix attributes to apply to the form"
+  )
+
+  def relationship_maker(assigns) do
+    ~H"""
+    <h3>
+      <%= @title %>
+    </h3>
+    <.simple_form :let={f} for={@for} id={@id}>
+      <.input field={f[:target_note_id]} type="text" autocomplete="off" />
+      <.input field={f[:relationship_type_id]} type="text" autocomplete="off" />
+      <.button phx-disable-with="Saving note...">Relate notes</.button>
+    </.simple_form>
+    """
+  end
 end
