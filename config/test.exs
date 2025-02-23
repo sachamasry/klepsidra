30bf3656c902bf2f8cd19e6b7dda76177d7601f7
sacha <sacha@sachamasry.com>
2025-02-23 14:37:59 +0000

    Updated `report_job` schema definitions, ensuring that all newly defined fields in the migration are available, for continued prototype development and testing.
    FossilOrigin-Name: d3b688e19cef7dd080aae88bdeca4ce44bf29b9ec4d6ced2acba113b3b150654

--- config/test.exs
+++ config/test.exs
@@ -10,6 +10,12 @@ config :klepsidra, Klepsidra.Repo,
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :klepsidra, Klepsidra.ReporterRepo,
+       database: Path.expand("../db/reporter_test.db", Path.dirname(__ENV__.file)),
+       pool_size: 5,
+       stacktrace: true,
+       show_sensitive_data_on_connection_error: false
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :klepsidra, KlepsidraWeb.Endpoint,
--- lib/klepsidra/reports/report_job.ex
+++ lib/klepsidra/reports/report_job.ex
@@ -1,14 +1,55 @@
 defmodule Klepsidra.ReportJob do
   @moduledoc """
+  Defines a schema for the `ReportJob` entity, a queue and audit trail of all
+  jobs, requesting the generation of a report.
   """
 
   use Ecto.Schema
+  import Ecto.Changeset
 
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @foreign_key_type Ecto.UUID
+
+  @type t :: %__MODULE__{
+          id: Ecto.UUID,
+          report_name: String.t(),
+          report_template: String.t(),
+          output_format: String.t(),
+          state: String.t(),
+          report_fingerprint: String.t(),
+          parameters: map(),
+          errors: map(),
+          meta: map(),
+          attempt: integer(),
+          max_attempts: integer(),
+          priority: integer(),
+          result_path: String.t(),
+          scheduled_at: String.t(),
+          attempted_at: String.t(),
+          attempted_by: map(),
+          cancelled_at: String.t(),
+          completed_at: String.t(),
+          discarded_at: String.t()
+        }
   schema "report_jobs" do
-    field :status, :string
+    field :report_name, :string
+    field :report_template, :string
+    field :output_format, :string
+    field :state, :string
     field :report_fingerprint, :string
     field :parameters, :string  # Stored as JSON string
+    field :errors, :map
+    field :meta, :map
+    field :attempt, :integer
+    field :max_attempts, :integer
+    field :priority, :integer
     field :result_path, :string
+    field :scheduled_at, :string
+    field :attempted_at, :string
+    field :attempted_by, :map
+    field :cancelled_at, :string
+    field :completed_at, :string
+    field :discarded_at, :string
 
     timestamps()
   end
