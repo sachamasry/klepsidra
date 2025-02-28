0d351a242a0afdd0acf18cf5ba5e9dd47f046e8f
sacha <sacha@sachamasry.com>
2025-02-28 21:56:00 +0000

    Implemented `compute_hash` function to compute _fingerprints_ of parameter maps and JSON data strings when creating reporter jobs.
    FossilOrigin-Name: c805f28763c92796a501d7d80c2913fdc79a254fa3b93c2ff0e4f2f02a7b6bb7

---
+++ lib/klepsidra/utilities/hash_functions.ex
@@ -0,0 +1,35 @@
+defmodule Klepsidra.Utilities.HashFunctions do
+  @moduledoc """
+  This module simplifies interfacing to Erlang's `:crypto` module, providing string hashing functions, for _fingerprinting_ purposes.
+  """
+
+
+  @doc """
+  Takes a message, hashing it with the [BLAKE2b](https://www.blake2.net/)
+  hashing function, encoding the resultant binary into a base-16 encoded
+  lowercased string, with no padding.
+
+  The message, passed through the `data` parameter, can be a string or map.
+
+  ## Examples
+
+  		iex> compute_hash("Hello, world!")
+  		"a2764d133a16816b5847a737a786f2ece4c148095c5faa73e24b4cc5d666c3e45ec271504e14dc6127ddfce4e144fb23b91a6f7b04b53d695502290722953b0f"
+
+  		iex> compute_hash(%{key: "value"})
+  		"8859ef88bbc5a0f858d0971364d9c70d93186e6c4b5655833f18a17e750c7558ac7f7cd4c48a86f279f33a5bb3635a11b23642f016ab58d4d8f4457a20110525"
+
+  		iex> compute_hash()
+  		** (UndefinedFunctionError)
+  """
+  @spec compute_hash(message :: bitstring() | map()) :: any()
+  def compute_hash(message) when is_bitstring(message) do
+    :crypto.hash(:blake2b, message)
+    |> Base.encode16(padding: false, case: :lower)
+  end
+
+  def compute_hash(message) when is_map(message) do
+    :crypto.hash(:blake2b, :erlang.term_to_binary(message))
+    |> Base.encode16(padding: false, case: :lower)
+  end
+end
