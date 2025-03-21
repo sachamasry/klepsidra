8a995fce783e32cff413c68e3be6a0d9b1f58acb
sacha <sacha@sachamasry.com>
2025-03-21 22:52:38 +0000

    Expanded `report_table_manager` module, developing a temporary table-naming function, temporary table creation and deletion, along with a start of work on an automated table insertion function, in service of creating an MVP report generation structure on the requesting application side.
    FossilOrigin-Name: 4c2ec9ec4547c105641b06a5ad3a7d65ed3a279422cda43ed0a183287da1f1c9

--- lib/klepsidra/reports/report_table_manager.ex
+++ lib/klepsidra/reports/report_table_manager.ex
@@ -4,7 +4,55 @@ defmodule Klepsidra.Reporting.ReportTableManager do
   tables. 
   """
 
-  # alias Klepsidra.ReporterRepo
+  alias Klepsidra.ReporterRepo
+
+  @doc """
+  Generate a unique temporary table to hold input data for a single report.
+  Following the successful generation of that report, reaching the maximum
+  permitted attempts at generation, cancellation or discarding of the job,
+  this table will be dropped so as not to become part of a _memory leak_ as
+  well as to safeguard data confidentiality and privacy.
+  """
+  @spec create_temporary_table(
+          report_job_id :: Ecto.UUID.t(),
+          report_name :: String.t(),
+          dataset :: [map(), ...]
+        ) :: String.t()
+  def create_temporary_table(report_job_id, report_name, dataset)
+      when is_bitstring(report_name) and is_list(dataset) and dataset != [] do
+    table_name = construct_table_name(report_job_id, report_name)
+
+    # Infer field types from first record
+    columns = infer_column_types(List.first(dataset))
+
+    # Generate CREATE TABLE SQL statement
+    create_table_sql_statement = generate_create_table_sql(table_name, columns)
+
+    # Execute table creation
+    ReporterRepo.query!(create_table_sql_statement)
+
+    # Insert data
+    insert_dataset_into_table(table_name, dataset, columns)
+  end
+
+  @spec delete_temporary_table(report_job_id :: Ecto.UUID.t(), report_name :: String.t()) ::
+          Exqlite.Result.t()
+  def delete_temporary_table(report_job_id, report_name)
+      when is_bitstring(report_job_id) and is_bitstring(report_name) do
+    table_name = construct_table_name(report_job_id, report_name)
+
+    # Generate DROP TABLE SQL statement
+    drop_table_sql_statement = generate_drop_table_sql(table_name)
+
+    # Execute table deletion
+    ReporterRepo.query!(drop_table_sql_statement)
+  end
+
+  @spec construct_table_name(report_job_id :: Ecto.UUID.t(), report_name :: String.t()) ::
+          bitstring()
+  defp construct_table_name(report_job_id, report_name) do
+    "report_#{report_name}_#{report_job_id |> String.replace("-", "_")}"
+  end
 
   @doc """
   Takes a single `record` map, inspect all its fields, or _keys_, returning a
@@ -12,32 +60,63 @@ defmodule Klepsidra.Reporting.ReportTableManager do
   deconstruction will be used to define a temporary database table modeled to
   hold the requested report's input dataset.
   """
-  @spec infer_columns(record :: any()) :: [{atom(), bitstring()}, ...]
-  def infer_columns(record) when is_map(record) do
+  @spec infer_column_types(record :: any()) :: [{atom(), bitstring()}, ...]
+  def infer_column_types(record) when is_map(record) do
     record
     |> Map.to_list()
     |> Enum.map(fn {field, value} ->
-      type = infer_type(value)
+      type = infer_column_type(value)
       {field, type}
     end)
   end
 
-  @spec infer_type(value :: any()) :: String.t()
-  defp infer_type(value) when is_nil(value), do: "TEXT"
-  defp infer_type(true), do: "BOOLEAN"
-  defp infer_type(false), do: "BOOLEAN"
-  defp infer_type(value) when is_boolean(value), do: "BOOLEAN"
-  defp infer_type(value) when is_atom(value), do: "ATOM"
-  defp infer_type(value) when is_float(value), do: "REAL"
-  defp infer_type(value) when is_integer(value), do: "INTEGER"
-  defp infer_type(value) when is_number(value), do: "INTEGER"
-  defp infer_type(value) when is_binary(value), do: "TEXT"
-  defp infer_type(value) when is_list(value), do: "JSON"
-  defp infer_type(value) when is_tuple(value), do: "TEXT"
-  defp infer_type(value) when is_struct(value, NaiveDateTime), do: "TEXT"
-  defp infer_type(value) when is_struct(value, DateTime), do: "TEXT"
-  defp infer_type(value) when is_struct(value, Date), do: "TEXT"
-  defp infer_type(value) when is_map(value), do: "MAP"
+  @spec infer_column_type(value :: any()) :: String.t()
+  defp infer_column_type(value) when is_nil(value), do: "TEXT"
+  defp infer_column_type(true), do: "BOOLEAN"
+  defp infer_column_type(false), do: "BOOLEAN"
+  defp infer_column_type(value) when is_boolean(value), do: "BOOLEAN"
+  defp infer_column_type(value) when is_atom(value), do: "ATOM"
+  defp infer_column_type(value) when is_float(value), do: "REAL"
+  defp infer_column_type(value) when is_integer(value), do: "INTEGER"
+  defp infer_column_type(value) when is_number(value), do: "INTEGER"
+  defp infer_column_type(value) when is_binary(value), do: "TEXT"
+  defp infer_column_type(value) when is_list(value), do: "JSON"
+  defp infer_column_type(value) when is_tuple(value), do: "TEXT"
+  defp infer_column_type(value) when is_struct(value, NaiveDateTime), do: "TEXT"
+  defp infer_column_type(value) when is_struct(value, DateTime), do: "TEXT"
+  defp infer_column_type(value) when is_struct(value, Date), do: "TEXT"
+  defp infer_column_type(value) when is_map(value), do: "MAP"
   # Fallback type
-  defp infer_type(_), do: "TEXT"
+  defp infer_column_type(_), do: "TEXT"
+
+  @spec generate_create_table_sql(table_name :: String.t(), [{atom(), String.t()}]) :: String.t()
+  defp generate_create_table_sql(table_name, columns) do
+    column_definitions =
+      columns
+      |> Enum.map(fn {field, type} -> "#{field} #{type}" end)
+      |> Enum.join(", ")
+
+    "CREATE TABLE IF NOT EXISTS #{table_name} (#{column_definitions}) WITHOUT ROWID;"
+  end
+
+  @spec generate_drop_table_sql(table_name :: String.t()) :: String.t()
+  defp generate_drop_table_sql(table_name) do
+    "DROP TABLE IF EXISTS #{table_name};"
+  end
+
+  defp insert_dataset_into_table(table_name, dataset, columns) do
+    column_names = columns |> Enum.map(fn {field, _} -> field end)
+
+    # <>
+    insert_sql =
+      "INSERT INTO #{table_name} (#{Enum.join(column_names, ", ")}) VALUES "
+
+    # Enum.map(dataset, fn record ->
+    #   values = Enum.map(column_names, &"'#{Map.get(record, &1)}'")
+    #   "(#{Enum.join(values, ", ")})"
+    # end))
+    # |> Enum.join(", ")
+
+    # Repo.query!(insert_sql)
+  end
 end
