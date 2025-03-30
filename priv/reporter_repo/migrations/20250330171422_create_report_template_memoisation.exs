976f5f44bbb4ddd22578f0b85b8b77cd90972df4
sacha <sacha@sachamasry.com>
2025-03-30 16:39:02 +0000

    Created a new migration, defining a new table, `_create_report_template_memoisation`, to store unique report template file paths, and their compiled binary objects in a `BLOB` field. Upon first invocation, there will be no matching record for the template in this table, subsequently a record will record the template file path, its last modification date/time stamp, and a unique hash of the input template file, as well as the compiled binary representation, saving the time and system resources needed to recompile the template upon every request.
    FossilOrigin-Name: 2711c226e679fee08b6edd96b936530445be1c20b964197887572ea51b7d11e6

---
+++ priv/reporter_repo/migrations/20250330171422_create_report_template_memoisation.exs
@@ -0,0 +1,33 @@
+defmodule Klepsidra.ReporterRepo.Migrations.CreateReportTemplateMemoisation do
+  use Ecto.Migration
+
+  def change do
+    create table(:report_templates_memoisation,
+             primary_key: false,
+             comment:
+               "Report templates is a memoisation table, storing compiled report templates, removing the need to [expensively] compile report templates at every report generation."
+           ) do
+      add :template_path, :string,
+        primary_key: true,
+        null: false,
+        comment:
+          "Absolute report template path-based primary key. Each unique report variant will be backed by precisely one report template, located at a necessarily unique file path, making it a good primary key candidate."
+
+      add :template_hash, :string,
+        null: false,
+        comment:
+          "Unique SHA-256 hash of the template file. If the current file hash maches the recorded hash, then use the memoised compiled template, preventing an expensive (half-second and above) template file compilation."
+
+      add :compiled_report_template, :binary,
+        null: false,
+        comment: "Binary BLOB field, storing the in-memory compiled report template."
+
+      add :last_modified, :utc_datetime_usec,
+        null: false,
+        comment:
+          "Template file's last modified datetime stamp, in microseconds. A primary check for template changes, if the `last_modified` stamp is the same, then not even the hash function need be invoked, saving further processing resources."
+
+      timestamps()
+    end
+  end
+end
